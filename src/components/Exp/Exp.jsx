bda6850a2133c0d39310079a97ef9f797aa659cd
Ardonit Saliji <ardonit.1980@gmail.com>
2022-07-05 03:09:32 +0200

    changed exp

--- src/components/Exp/Exp.jsx
+++ src/components/Exp/Exp.jsx
@@ -36,7 +36,7 @@ const Exp = () => {
             <article className='experience__details'>
               <BsPatchCheckFill className='experience__details-icon' />
               <div>
-                <h4>Bootstrap</h4>
+                <h4>Git</h4>
                 <small className='text-light'>Intermediate</small>
               </div>
             </article>

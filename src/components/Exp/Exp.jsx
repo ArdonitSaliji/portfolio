e82122d0ab05fef5d8d823e7d4b0fa0a92b65a74
Ardonit Saliji <ardonit.1980@gmail.com>
2023-01-09 04:09:32 +0100

    changed exp component

--- src/components/Exp/Exp.jsx
+++ src/components/Exp/Exp.jsx
@@ -1,64 +1,71 @@
-import React from 'react'
-import '../../index.css'
-import './Exp.css'
-import { BsPatchCheckFill } from 'react-icons/bs'
+import React from "react";
+import "../../index.css";
+import "./Exp.css";
+import { BsPatchCheckFill } from "react-icons/bs";
 
 const Exp = () => {
   return (
-    <section id='experience'>
+    <section id="experience">
       <h5>What Skills I Have</h5>
       <h2>My Experience</h2>
-      <div className='container experience__container'>
-        <div className='experience__frontend'>
+      <div className="container experience__container">
+        <div className="experience__frontend">
           <h3>Frontend Development</h3>
-          <div className='experience__content'>
-            <article className='experience__details'>
-              <BsPatchCheckFill className='experience__details-icon' />
+          <div className="experience__content">
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon" />
               <div>
                 <h4>React.js</h4>
-                <small className='text-light'>Experienced</small>
+                <small className="text-light">Experienced</small>
               </div>
             </article>
-            <article className='experience__details'>
-              <BsPatchCheckFill className='experience__details-icon' />
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon" />
               <div>
                 <h4>Redux</h4>
-                <small className='text-light'>Intermediate</small>
+                <small className="text-light">Intermediate</small>
               </div>
             </article>
-            <article className='experience__details'>
-              <BsPatchCheckFill className='experience__details-icon' />
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon" />
               <div>
                 <h4>JavaScript</h4>
-                <small className='text-light'>Experienced</small>
+                <small className="text-light">Experienced</small>
               </div>
             </article>
-            <article className='experience__details'>
-              <BsPatchCheckFill className='experience__details-icon' />
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon" />
               <div>
                 <h4>TypeScript</h4>
-                <small className='text-light'>Experienced</small>
+                <small className="text-light">Experienced</small>
               </div>
             </article>
-            <article className='experience__details'>
-              <BsPatchCheckFill className='experience__details-icon' />
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon" />
               <div>
                 <h4>Git</h4>
-                <small className='text-light'>Experienced</small>
+                <small className="text-light">Experienced</small>
               </div>
             </article>
-            <article className='experience__details'>
-              <BsPatchCheckFill className='experience__details-icon' />
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon" />
               <div>
                 <h4>HTML and CSS</h4>
-                <small className='text-light'>Experienced</small>
+                <small className="text-light">Experienced</small>
               </div>
             </article>
-            <article className='experience__details'>
-              <BsPatchCheckFill className='experience__details-icon' />
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>Sass</h4>
+                <small className="text-light">Intermediate</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon" />
               <div>
                 <h4>Jquery</h4>
-                <small className='text-light'>Experienced</small>
+                <small className="text-light">Experienced</small>
               </div>
             </article>
           </div>
@@ -66,71 +73,71 @@ const Exp = () => {
 
         {/* Backend */}
 
-        <div className='experience__backend'>
+        <div className="experience__backend">
           <h3>Backend Development</h3>
-          <div className='experience__content'>
-            <article className='experience__details'>
-              <BsPatchCheckFill className='experience__details-icon' />
+          <div className="experience__content">
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon" />
               <div>
                 <h4>PHP</h4>
-                <small className='text-light'>Intermediate</small>
+                <small className="text-light">Intermediate</small>
               </div>
             </article>
-            <article className='experience__details'>
-              <BsPatchCheckFill className='experience__details-icon' />
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon" />
               <div>
                 <h4>MySQL</h4>
-                <small className='text-light'>Intermediate</small>
+                <small className="text-light">Intermediate</small>
               </div>
             </article>
-            <article className='experience__details'>
-              <BsPatchCheckFill className='experience__details-icon' />
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon" />
               <div>
                 <h4>Node.js and Express.js</h4>
-                <small className='text-light'>Intermediate</small>
+                <small className="text-light">Intermediate</small>
               </div>
             </article>
 
-            <article className='experience__details'>
-              <BsPatchCheckFill className='experience__details-icon' />
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon" />
               <div>
                 <h4>MongoDB</h4>
-                <small className='text-light'>Intermediate</small>
+                <small className="text-light">Intermediate</small>
               </div>
             </article>
-            <article className='experience__details'>
-              <BsPatchCheckFill className='experience__details-icon' />
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon" />
               <div>
                 <h4>Python</h4>
-                <small className='text-light'>Beginner</small>
+                <small className="text-light">Beginner</small>
               </div>
             </article>
-            <article className='experience__details'>
-              <BsPatchCheckFill className='experience__details-icon' />
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon" />
               <div>
                 <h4>C</h4>
-                <small className='text-light'>Beginner</small>
+                <small className="text-light">Beginner</small>
               </div>
             </article>
-            <article className='experience__details'>
-              <BsPatchCheckFill className='experience__details-icon' />
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon" />
               <div>
                 <h4>Microsoft Azure and Transact-SQL</h4>
-                <small className='text-light'>Intermediate</small>
+                <small className="text-light">Intermediate</small>
               </div>
             </article>
-            <article className='experience__details'>
-              <BsPatchCheckFill className='experience__details-icon' />
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon" />
               <div>
                 <h4>REST Api</h4>
-                <small className='text-light'>Intermediate</small>
+                <small className="text-light">Intermediate</small>
               </div>
             </article>
           </div>
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Exp
+export default Exp;
--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,41 +1,41 @@
 /* eslint-disable jsx-a11y/alt-text */
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React from 'react'
-import { AiOutlineClose } from 'react-icons/ai'
-import { Swiper, SwiperSlide } from 'swiper/react'
-import { Navigation, Pagination, Scrollbar, A11y } from 'swiper'
-import './portfolio.css'
-import 'swiper/css'
-import 'swiper/css/navigation'
-import 'swiper/css/pagination'
-import 'swiper/css/scrollbar'
-import data from './data'
+import React from "react";
+import { AiOutlineClose } from "react-icons/ai";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination, Scrollbar, A11y } from "swiper";
+import "./portfolio.css";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+import data from "./data";
 
 const Portfolio = () => {
-  const [popup, setPopup] = React.useState(false)
-  const [popupImage, setPopupImage] = React.useState('')
+  const [popup, setPopup] = React.useState(false);
+  const [popupImage, setPopupImage] = React.useState("");
   const images = (image) =>
     image.map((img, index) => (
       <SwiperSlide key={index}>
         <img
           src={img}
           onClick={() => {
-            setPopup((prev) => !prev)
-            setPopupImage(img)
+            setPopup((prev) => !prev);
+            setPopupImage(img);
           }}
-          alt=''
+          alt=""
         />
       </SwiperSlide>
-    ))
+    ));
   return (
-    <section id='portfolio'>
+    <section id="portfolio">
       <h5>My Recent Work</h5>
       <h2>Portfolio</h2>
-      <div className='container portfolio__container'>
+      <div className="container portfolio__container">
         {data.map(({ image, title, github, demo }, index) => {
           return (
-            <article key={index} className='portfolio__item'>
-              <div id='item' className='portfolio__item-img'>
+            <article key={index} className="portfolio__item">
+              <div id="item" className="portfolio__item-img">
                 <Swiper
                   modules={[Navigation, Pagination, Scrollbar, A11y]}
                   spaceBetween={5}
@@ -47,26 +47,40 @@ const Portfolio = () => {
                 </Swiper>
               </div>
               <h3>{title}</h3>
-              <div className='portfolio__item-cta'>
-                <a href={github} target='_blank' rel='noreferrer' className='btn'>
+              <div className="portfolio__item-cta">
+                <a
+                  href={github}
+                  target="_blank"
+                  rel="noreferrer"
+                  className="btn"
+                >
                   Github
                 </a>
-                <a href={demo} target='_blank' rel='noreferrer' className='btn btn-primary'>
+                <a
+                  href={demo}
+                  target="_blank"
+                  rel="noreferrer"
+                  className="btn btn-primary"
+                >
                   Live Demo
                 </a>
               </div>
             </article>
-          )
+          );
         })}
 
-        <div className={popup ? 'popup active' : 'popup'}>
-          <AiOutlineClose className='x' onClick={() => setPopup(false)} color='white' />
+        <div className={popup ? "popup active" : "popup"}>
+          <AiOutlineClose
+            className="x"
+            onClick={() => setPopup(false)}
+            color="white"
+          />
 
-          <img className='popup-img' src={popupImage}></img>
+          <img className="popup-img" src={popupImage}></img>
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Portfolio
+export default Portfolio;

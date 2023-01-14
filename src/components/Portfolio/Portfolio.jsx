db6daaf7b4f3e65a92adf9177ccd37fe2c87fd76
Ardonit Saliji <ardonit.1980@gmail.com>
2023-01-14 16:18:08 +0100

    Show/Hide more projects

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable jsx-a11y/alt-text */
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React from "react";
+import { useEffect, useLayoutEffect, useState } from "react";
 import { AiOutlineClose } from "react-icons/ai";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation, Pagination, Scrollbar, A11y } from "swiper";
@@ -9,11 +9,32 @@ import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "swiper/css/scrollbar";
-import data from "./data";
+import data, { more } from "./data";
 
 const Portfolio = () => {
-  const [popup, setPopup] = React.useState(false);
-  const [popupImage, setPopupImage] = React.useState("");
+  const [popup, setPopup] = useState(false);
+  const [popupImage, setPopupImage] = useState("");
+  const [moreProjects, setMoreProjects] = useState(false);
+
+  useLayoutEffect(() => {
+    if (moreProjects) {
+      let el = document.querySelector(".portfolio__container.more");
+      let height = el.scrollHeight;
+      el.style.maxHeight = height + "px";
+      el.style.visibility = "visible";
+
+      document.querySelector(".portfolio__container.more").style.transform =
+        "translateY(0%)";
+    } else {
+      let el = document.querySelector(".portfolio__container.more");
+      el.style.maxHeight = 0 + "px";
+      el.style.visibility = "hidden";
+
+      document.querySelector(".portfolio__container.more").style.transform =
+        "translateY(-200%)";
+    }
+  }, [moreProjects]);
+
   const images = (image) =>
     image.map((img, index) => (
       <SwiperSlide key={index}>
@@ -77,6 +98,64 @@ const Portfolio = () => {
           />
 
           <img className="popup-img" src={popupImage}></img>
+        </div>
+      </div>
+      <p
+        className="show-more"
+        onClick={() => {
+          setMoreProjects((prev) => !prev);
+        }}
+      >
+        Show More Projects...
+      </p>
+      <div className="more-projects">
+        <div className="container portfolio__container more">
+          {more.map(({ image, title, github, demo }, index) => {
+            return (
+              <article key={index} className="portfolio__item">
+                <div id="item" className="portfolio__item-img">
+                  <Swiper
+                    modules={[Navigation, Pagination, Scrollbar, A11y]}
+                    spaceBetween={5}
+                    slidesPerView={1}
+                    navigation
+                    pagination={{ clickable: true }}
+                  >
+                    {images(image)}
+                  </Swiper>
+                </div>
+                <h3>{title}</h3>
+                <div className="portfolio__item-cta">
+                  <a
+                    href={github}
+                    target="_blank"
+                    rel="noreferrer"
+                    className="btn"
+                  >
+                    Github
+                  </a>
+                  <a
+                    href={demo}
+                    target="_blank"
+                    rel="noreferrer"
+                    className="btn btn-primary"
+                  >
+                    Live Demo
+                  </a>
+                </div>
+              </article>
+            );
+          })}
+
+          <div className={popup ? "popup active" : "popup"}>
+            <AiOutlineClose
+              className="x"
+              onClick={() => setPopup(false)}
+              color="white"
+            />
+
+            <img className="popup-img" src={popupImage}></img>
+          </div>
         </div>
       </div>
     </section>
--- src/components/Portfolio/data.js
+++ src/components/Portfolio/data.js
@@ -1,55 +1,116 @@
 const data = [
   {
-    name: 'chess',
-    image: [require('../../images/chess1.png'), require('../../images/chess2.png')],
-    title: 'Chess Game',
-    github: 'https://github.com/ArdonitSaliji/TypeScript/tree/main/chess-project',
-    demo: 'https://ardonitsaliji.github.io/chess-project/',
+    name: "Sociopedia",
+    image: [
+      require("../../images/socio.png"),
+      require("../../images/socio2.png"),
+      require("../../images/socio3.png"),
+    ],
+    title: "Sociopedia (Social Media Website)",
+    github: "https://github.com/ArdonitSaliji/social-media",
   },
   {
-    name: 'Jewerelly Store (In Progress)',
-    image: [require('../../images/jewerelly2.png'), require('../../images/jewerelly1.png')],
-    title: 'Gem  Store',
-    github: 'https://github.com/ArdonitSaliji/JavaScript/tree/main/jewerelly-project',
+    name: "chess",
+    image: [
+      require("../../images/chess1.png"),
+      require("../../images/chess2.png"),
+    ],
+    title: "Chess Game",
+    github:
+      "https://github.com/ArdonitSaliji/TypeScript/tree/main/chess-project",
+    demo: "https://ardonitsaliji.github.io/chess-project/",
+  },
+  {
+    name: "Jewerelly Store (In Progress)",
+    image: [
+      require("../../images/jewerelly2.png"),
+      require("../../images/jewerelly1.png"),
+    ],
+    title: "Gem  Store",
+    github:
+      "https://github.com/ArdonitSaliji/JavaScript/tree/main/jewerelly-project",
     // demo: 'https://ardonitsaliji.github.io/jewerelly-store/',
   },
   {
-    name: 'snake',
-    image: [require('../../images/snake-game1.png'), require('../../images/snake-game2.png')],
-    title: 'Snake Game',
-    github: 'https://github.com/ArdonitSaliji/JavaScript/tree/main/snake-project',
-    demo: 'https://ardonitsaliji.github.io/snake-game/',
-  },
-  {
-    name: 'db-management',
+    name: "snake",
     image: [
-      require('../../images/db-manag1.png'),
-      require('../../images/db-manag2.png'),
-      require('../../images/db-manag3.png'),
-      require('../../images/db-manag4.png'),
+      require("../../images/snake-game1.png"),
+      require("../../images/snake-game2.png"),
     ],
-    title: 'Database User Management',
-    github: 'https://github.com/ArdonitSaliji/db-user-management',
-    demo: 'https://ardonitsaliji.github.io/db-user-management/',
+    title: "Snake Game",
+    github:
+      "https://github.com/ArdonitSaliji/JavaScript/tree/main/snake-project",
+    demo: "https://ardonitsaliji.github.io/snake-game/",
   },
   {
-    name: 'Modern UI/UX',
+    name: "db-management",
     image: [
-      require('../../images/modern1.png'),
-      require('../../images/modern2.png'),
-      require('../../images/modern3.png'),
-      require('../../images/modern4.png'),
-      require('../../images/modern5.png'),
+      require("../../images/db-manag1.png"),
+      require("../../images/db-manag2.png"),
+      require("../../images/db-manag3.png"),
+      require("../../images/db-manag4.png"),
     ],
-    title: 'GPT-3 OpenAI (Modern UI/UX)',
-    github: 'https://github.com/ArdonitSaliji/modern-ui-ux',
-    demo: 'https://ardonitsaliji.github.io/modern-ui-ux/',
+    title: "Database User Management",
+    github: "https://github.com/ArdonitSaliji/db-user-management",
+    demo: "https://ardonitsaliji.github.io/db-user-management/",
   },
   {
-    name: 'meme',
-    image: [require('../../images/jokes3.png'), require('../../images/jokes.png')],
-    title: 'Meme Editor Website',
-    github: 'https://github.com/ArdonitSaliji/JavaScript/tree/main/memes-project',
+    name: "Modern UI/UX",
+    image: [
+      require("../../images/modern1.png"),
+      require("../../images/modern2.png"),
+      require("../../images/modern3.png"),
+      require("../../images/modern4.png"),
+      require("../../images/modern5.png"),
+    ],
+    title: "GPT-3 OpenAI (Modern UI/UX)",
+    github: "https://github.com/ArdonitSaliji/modern-ui-ux",
+    demo: "https://ardonitsaliji.github.io/modern-ui-ux/",
   },
 ];
 export default data;
+
+export const more = [
+  {
+    name: "meme",
+    image: [
+      require("../../images/jokes3.png"),
+      require("../../images/jokes.png"),
+    ],
+    title: "Meme Editor Website",
+    github:
+      "https://github.com/ArdonitSaliji/JavaScript/tree/main/memes-project",
+  },
+
+  {
+    name: "Jewerelly Store (In Progress)",
+    image: [
+      require("../../images/jewerelly2.png"),
+      require("../../images/jewerelly1.png"),
+    ],
+    title: "Gem  Store",
+    github:
+      "https://github.com/ArdonitSaliji/JavaScript/tree/main/jewerelly-project",
+    // demo: 'https://ardonitsaliji.github.io/jewerelly-store/',
+  },
+
+  {
+    name: "Modern UI/UX",
+    image: [
+      require("../../images/modern1.png"),
+      require("../../images/modern2.png"),
+      require("../../images/modern3.png"),
+      require("../../images/modern4.png"),
+      require("../../images/modern5.png"),
+    ],
+    title: "GPT-3 OpenAI (Modern UI/UX)",
+    github: "https://github.com/ArdonitSaliji/modern-ui-ux",
+    demo: "https://ardonitsaliji.github.io/modern-ui-ux/",
+  },
+  // {
+  //   name: 'meme',
+  //   image: [require('../../images/jokes3.png'), require('../../images/jokes.png')],
+  //   title: 'Meme Editor Website',
+  //   github: 'https://github.com/ArdonitSaliji/JavaScript/tree/main/memes-project',
+  // },
+];
